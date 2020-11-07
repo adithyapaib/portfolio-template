e867f2df9e884a26517415293d1f34f959b163a8
Adithya Pai B <paiadithya26@gmail.com>
2020-11-07 16:02:48 +0530

    4 pm commit

---
+++ asset/js/index.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', function(e) {
+    document.getElementsByTagName('meta')["theme-color"].content = "#003140";0
+  })  
+  var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    backSpeed:50,
+    typeSpeed:60,
+    loop:true,
+  });
